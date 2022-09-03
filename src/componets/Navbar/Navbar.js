d090ec5eccb516c0b649ab452da7b8078b93a61a
loganmtk <manuel.garin@utacapulco.edu.mx>
2022-09-03 17:38:54 -0500

    first commit

---
+++ src/componets/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./Navbar.styles";
+
+
+const Navbar = () => {
+    return (
+        <S.ContainerNavbar>
+            nuestro primer container
+        </S.ContainerNavbar>
+    );
+}
+export default Navbar;
